ac8bf13e8a5f38ccc87b2c146d9e0d9351df1379
david-chaves-martins <nynyx23@gmail.com>
2026-06-16 22:51:06 -0300

    Implementa SAGSA-Lite com repositorio Excel fake

--- src/sagsa/Main.java
+++ src/sagsa/Main.java
@@ -8,9 +8,7 @@ import sagsa.view.TerminalInterface;
 public class Main {
     public static void main(String[] args) {
         // 1. Instancia o subsistema de persistência de dados
-        RepositoryContrato bancoDados = new FakeExcelRepository();
-
-        // 2. Cria o motor injetando o contrato de dados
+        RepositoryContrato bancoDados = new FakeExcelRepository();        // 2. Cria o motor injetando o contrato de dados
         GeradorEstrategiaCore core = new GeradorEstrategiaCore(bancoDados);
 
         // 3. Monta a interface conectando ao core
